86ef85179da13ae5cf9b91ef58c1db8d2b171dfd
Hunter <hmvitous@yahoo.com>
2020-02-28 16:47:17 +0100

    Updates cypress tests. Adds placeholder images to visually see a layout

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,11 +3,22 @@ import React from 'react'
 const Main = () => {
     return (
         <div>
-            <div className='buttons'>
+            <div className='banner'>
+                <img id='banner-image' src='../dist/images/no-image.jpg'></img>
+            </div>
+           
+            <div className='image'>
+                <img id='rock-image' src='../dist/images/no-image.jpg'></img>
+                <img id='paper-image' src='../dist/images/no-image.jpg'></img>
+                <img id='scissors-image' src='../dist/images/no-image.jpg'></img>
+            </div>
+            
+            <div className='button'>
                 <button id='rock-button'>Rock</button>
                 <button id='paper-button'>Paper</button>
                 <button id='scissors-button'>Scissors</button>
             </div>
+
         </div>
        
     )
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require("webpack");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/app.js",
   mode: "development",
   module: {
     rules: [
