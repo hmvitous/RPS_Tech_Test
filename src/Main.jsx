96253f3ddafda6f6baba9451af2ea74cb156e9a1
Hunter <hmvitous@yahoo.com>
2020-02-29 15:31:51 +0100

    Adds the 'VS.' in the middle of the screen

--- src/Main.jsx
+++ src/Main.jsx
@@ -9,6 +9,7 @@ const Main = () => {
       <div>
         <div className="title">
           <h1 id="player-title">Player</h1>
+          <h3 id="vs">VS.</h3>
           <h1 id="computer-title">Computer</h1>
         </div>
       </div>
