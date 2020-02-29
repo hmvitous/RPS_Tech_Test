909ea60886ab77c887272d6606a35b27893b09ea
Hunter <hmvitous@yahoo.com>
2020-02-29 15:23:40 +0100

    Creates player titles and positions the titles

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,27 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const Main = () => {
-    return (
-        <div>
-            <div className='banner'>
-                <img id='banner-image' src='../dist/images/no-image.jpg'></img>
-            </div>
-           
-            <div className='image'>
-                <img id='rock-image' src='../dist/images/no-image.jpg'></img>
-                <img id='paper-image' src='../dist/images/no-image.jpg'></img>
-                <img id='scissors-image' src='../dist/images/no-image.jpg'></img>
-            </div>
-            
-            <div className='button'>
-                <button id='rock-button'>Rock</button>
-                <button id='paper-button'>Paper</button>
-                <button id='scissors-button'>Scissors</button>
-            </div>
-
+  return (
+    <div>
+      <div className="banner">
+        <img id="banner-image" src="../dist/images/no-image.jpg"></img>
+      </div>
+      <div>
+        <div className="title">
+          <h1 id="player-title">Player</h1>
+          <h1 id="computer-title">Computer</h1>
         </div>
-       
-    )
-}    
+      </div>
 
-export default Main
+      <div className="image">
+        <img id="rock-image" src="../dist/images/no-image.jpg"></img>
+        <img id="paper-image" src="../dist/images/no-image.jpg"></img>
+        <img id="scissors-image" src="../dist/images/no-image.jpg"></img>
+      </div>
+
+      <div className="button">
+        <button id="rock-button">Rock</button>
+        <button id="paper-button">Paper</button>
+        <button id="scissors-button">Scissors</button>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
