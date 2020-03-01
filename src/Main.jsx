3d8ec7796759429eb0a2dc89dd3f205c08aaa1f0
Hunter <hmvitous@yahoo.com>
2020-03-01 13:52:02 +0100

    Edits files to make the site load again

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,29 +1,28 @@
-import React from "react";
+import React from 'react';
 
 const Main = () => {
   return (
     <div>
-      <div className="banner">
-        <img id="banner-image" src="../dist/images/no-image.jpg"></img>
+      <div className='banner'>
+        <img id='banner-image' src='../dist/images/no-image.jpg'></img>
       </div>
       <div>
-        <div className="title">
-          <h1 id="player-title">Player</h1>
-          <h3 id="vs">VS.</h3>
-          <h1 id="computer-title">Computer</h1>
+        <div className='title'>
+          <h1 id='player-title'>Player</h1>
+          <h3 id='vs'>VS.</h3>
+          <h1 id='computer-title'>Computer</h1>
         </div>
       </div>
 
-      <div className="image">
-        <img id="rock-image" src="../dist/images/no-image.jpg"></img>
-        <img id="paper-image" src="../dist/images/no-image.jpg"></img>
-        <img id="scissors-image" src="../dist/images/no-image.jpg"></img>
+      <div className='image'>
+        <img id='player-image' src='../dist/images/no-image.jpg'></img>
+        <img id='computer-image' src='../dist/images/no-image.jpg'></img>
       </div>
 
-      <div className="button">
-        <button id="rock-button">Rock</button>
-        <button id="paper-button">Paper</button>
-        <button id="scissors-button">Scissors</button>
+      <div className='button'>
+        <button id='rock-button'>Rock</button>
+        <button id='paper-button'>Paper</button>
+        <button id='scissors-button'>Scissors</button>
       </div>
     </div>
   );
