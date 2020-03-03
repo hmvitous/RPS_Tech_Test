4c40cbf0f1b7d0cb37bdc93a1ba6c05af926e933
Hunter <hmvitous@yahoo.com>
2020-03-03 20:05:57 +0100

    Gets the button clicked to show the corresponding image

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,33 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import GameHeader from './GameHeader'
 import PlayerImage from './PlayerImage'
 
 
 
-const Main = () => {
+class Main extends Component {
 
-  var player1 = <PlayerImage type='Player' choice="1" id="1" />
+  // var player1 = <PlayerImage type='Player' choice="1" id="1" />
 
-  const onButtonClick = function (event) {
-    const clickedChoice = event.currentTarget.id
-    console.log ("clicked " + clickedChoice)
-    player1.choice = clickedChoice
+  constructor(props) {
+    super(props)
+    this.state = { player: <PlayerImage type='Player' choice="1" id="1" /> }
+    this.onButtonClick = this.onButtonClick.bind(this)
   }
 
-  return (
-    <div>
-      <GameHeader />
-      {player1}
-      <PlayerImage type='Player' choice="1" id="1" /> 
-      <img src="../dist/images/vs.png"></img>   
-      <PlayerImage type='Computer' choice="2" id="2" />
-      <div className='button'>
-        <button id='3' onClick={onButtonClick}>Rock</button>
-        <button id='1' onClick={onButtonClick}>Paper</button>
-        <button id='2' onClick={onButtonClick}>Scissors</button>
-      </div>
-    </div>
-  );
-};
+
+  onButtonClick(event) {
+    const clickedChoice = event.currentTarget.id
+    console.log ("clicked " + clickedChoice)
+    this.setState({ player: <PlayerImage type='Player' choice={clickedChoice} /> })
+    // player1 = <PlayerImage type='Player' choice={clickedChoice} id="1" />
+  }
+
+
+render () {
+    return (
+          <div>
+            <GameHeader />
+            {this.state.player}
+            
+            <img src="../dist/images/vs.png"></img>   
+            <PlayerImage type='Computer' choice="2" id="2" />
+            <div className='button'>
+              <button id='3' onClick={this.onButtonClick}>Rock</button>
+              <button id='1' onClick={this.onButtonClick}>Paper</button>
+              <button id='2' onClick={this.onButtonClick}>Scissors</button>
+            </div>
+          </div>
+    );
+  };
+}
 
 export default Main;
