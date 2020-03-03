c076dfe5e8de26c3f9ea37db620f0bfcd2e3707a
Hunter <hmvitous@yahoo.com>
2020-03-03 20:07:57 +0100

    Cleans up the code format

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,44 +1,44 @@
-import React, { Component } from 'react';
-import GameHeader from './GameHeader'
-import PlayerImage from './PlayerImage'
-
-
+import React, { Component } from "react";
+import GameHeader from "./GameHeader";
+import PlayerImage from "./PlayerImage";
 
 class Main extends Component {
-
-  // var player1 = <PlayerImage type='Player' choice="1" id="1" />
-
   constructor(props) {
-    super(props)
-    this.state = { player: <PlayerImage type='Player' choice="1" id="1" /> }
-    this.onButtonClick = this.onButtonClick.bind(this)
+    super(props);
+    this.state = { player: <PlayerImage type="Player" choice="1" id="1" /> };
+    this.onButtonClick = this.onButtonClick.bind(this);
   }
-
 
   onButtonClick(event) {
-    const clickedChoice = event.currentTarget.id
-    console.log ("clicked " + clickedChoice)
-    this.setState({ player: <PlayerImage type='Player' choice={clickedChoice} /> })
-    // player1 = <PlayerImage type='Player' choice={clickedChoice} id="1" />
+    const clickedChoice = event.currentTarget.id;
+    console.log("clicked " + clickedChoice);
+    this.setState({
+      player: <PlayerImage type="Player" choice={clickedChoice} />
+    });
   }
 
-
-render () {
+  render() {
     return (
-          <div>
-            <GameHeader />
-            {this.state.player}
-            
-            <img src="../dist/images/vs.png"></img>   
-            <PlayerImage type='Computer' choice="2" id="2" />
-            <div className='button'>
-              <button id='3' onClick={this.onButtonClick}>Rock</button>
-              <button id='1' onClick={this.onButtonClick}>Paper</button>
-              <button id='2' onClick={this.onButtonClick}>Scissors</button>
-            </div>
-          </div>
+      <div>
+        <GameHeader />
+        {this.state.player}
+
+        <img src="../dist/images/vs.png"></img>
+        <PlayerImage type="Computer" choice="2" id="2" />
+        <div className="button">
+          <button id="3" onClick={this.onButtonClick}>
+            Rock
+          </button>
+          <button id="1" onClick={this.onButtonClick}>
+            Paper
+          </button>
+          <button id="2" onClick={this.onButtonClick}>
+            Scissors
+          </button>
+        </div>
+      </div>
     );
-  };
+  }
 }
 
 export default Main;
